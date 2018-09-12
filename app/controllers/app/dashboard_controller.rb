d7e2cffcbfd14e584f9b85aa6f2cbbce0b5a1554
LeonardJAvi <informatic.leonardavila@gmail.com>
2018-09-12 16:32:43 -0400

    Add view dashboard client

---
+++ app/controllers/app/dashboard_controller.rb
@@ -0,0 +1,11 @@
+module App
+  # DashboardsController
+  class DashboardController < AppController
+    layout 'layouts/templates/application'
+    
+    def transfer_orders
+    
+    end
+
+  end
+end
