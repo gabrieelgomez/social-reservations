e7342ea6d8b2c7be00c85bd74f6b97f1962753c3
Gabriel Gómez <gagg1707@gmail.com>
2018-09-21 11:45:46 -0400

    try app crashed

--- app/controllers/app/reservations/transfers/transfers_controller.rb
+++ app/controllers/app/reservations/transfers/transfers_controller.rb
@@ -1,74 +1,75 @@
-module App::Reservations::Transfers
-  class TransfersController < CreateController
+module App
+  module Reservations
+    module Transfers
 
-    # POST /reservations
-    def session_reservation_transfer
-      session[:reservation] = KepplerTravel::Reservation.new(reservation_params)
-      session[:user]        = params[:user]
-      session[:invoice]     = params[:invoice]
-      session[:travellers]  = params[:travellers]
-      session[:reservationable]  = {type:'vehicle', id:params[:reservationable_id]}
-      redirect_to checkout_path(params[:lang], params[:currency])
-    end
+      class TransfersController < CreateController
+        # POST /reservations
+        def session_reservation_transfer
+          session[:reservation] = KepplerTravel::Reservation.new(reservation_params)
+          session[:user]        = params[:user]
+          session[:invoice]     = params[:invoice]
+          session[:travellers]  = params[:travellers]
+          session[:reservationable]  = {type:'vehicle', id:params[:reservationable_id]}
+          redirect_to checkout_path(params[:lang], params[:currency])
+        end
 
-    def create_reservation_transfer
-      @reservation = KepplerTravel::Reservation.new(session[:reservation])
-      find_or_create_user
-      @reservation.status = :pending
-      @reservation.user = @user
-      @reservation.reservationable = KepplerTravel::Vehicle.find session[:reservationable]['id']
-      @currency = session[:invoice].first['currency']
-      @price_total = @reservation.reservationable.price[@currency]
-      build_invoice
-      if @reservation.save!
-        create_travellers
-        ReservationMailer.transfer_status(@reservation, @user).deliver_now
-        redirect_to checkout_elp_redirect_path(@reservation.id, @reservation.invoice.id)
-      else
-        render :new
+        def create_reservation_transfer
+          @reservation = KepplerTravel::Reservation.new(session[:reservation])
+          find_or_create_user
+          @reservation.status = :pending
+          @reservation.user = @user
+          @reservation.reservationable = KepplerTravel::Vehicle.find session[:reservationable]['id']
+          @currency = session[:invoice].first['currency']
+          @price_total = @reservation.reservationable.price[@currency]
+          build_invoice
+          if @reservation.save!
+            create_travellers
+            ReservationMailer.transfer_status(@reservation, @user).deliver_now
+            redirect_to checkout_elp_redirect_path(@reservation.id, @reservation.invoice.id)
+          else
+            render :new
+          end
+
+        end
       end
 
     end
-
-
   end
 end
 
-# module App
-#   module Reservations
-#     module Transfers
+
+# module App::Reservations::Transfers
+#   class TransfersController < CreateController
 #
-#       class TransfersController < CreateController
-#         # POST /reservations
-#         def session_reservation_transfer
-#           session[:reservation] = KepplerTravel::Reservation.new(reservation_params)
-#           session[:user]        = params[:user]
-#           session[:invoice]     = params[:invoice]
-#           session[:travellers]  = params[:travellers]
-#           session[:reservationable]  = {type:'vehicle', id:params[:reservationable_id]}
-#           redirect_to checkout_path(params[:lang], params[:currency])
-#         end
+#     # POST /reservations
+#     def session_reservation_transfer
+#       session[:reservation] = KepplerTravel::Reservation.new(reservation_params)
+#       session[:user]        = params[:user]
+#       session[:invoice]     = params[:invoice]
+#       session[:travellers]  = params[:travellers]
+#       session[:reservationable]  = {type:'vehicle', id:params[:reservationable_id]}
+#       redirect_to checkout_path(params[:lang], params[:currency])
+#     end
 #
-#         def create_reservation_transfer
-#           @reservation = KepplerTravel::Reservation.new(session[:reservation])
-#           find_or_create_user
-#           @reservation.status = :pending
-#           @reservation.user = @user
-#           @reservation.reservationable = KepplerTravel::Vehicle.find session[:reservationable]['id']
-#           @currency = session[:invoice].first['currency']
-#           @price_total = @reservation.reservationable.price[@currency]
-#           build_invoice
-#           if @reservation.save!
-#             create_travellers
-#             ReservationMailer.transfer_status(@reservation, @user).deliver_now
-#             redirect_to checkout_elp_redirect_path(@reservation.id, @reservation.invoice.id)
-#           else
-#             render :new
-#           end
-#
-#         end
+#     def create_reservation_transfer
+#       @reservation = KepplerTravel::Reservation.new(session[:reservation])
+#       find_or_create_user
+#       @reservation.status = :pending
+#       @reservation.user = @user
+#       @reservation.reservationable = KepplerTravel::Vehicle.find session[:reservationable]['id']
+#       @currency = session[:invoice].first['currency']
+#       @price_total = @reservation.reservationable.price[@currency]
+#       build_invoice
+#       if @reservation.save!
+#         create_travellers
+#         ReservationMailer.transfer_status(@reservation, @user).deliver_now
+#         redirect_to checkout_elp_redirect_path(@reservation.id, @reservation.invoice.id)
+#       else
+#         render :new
 #       end
 #
 #     end
+#
+#
 #   end
 # end
