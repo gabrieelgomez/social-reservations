dafd8e4a97c2994f8bfb9b36bee029b393c99e54
Gabriel Gómez <gagg1707@gmail.com>
2018-10-17 14:36:19 -0400

    d

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,6 +40,12 @@ setting = Setting.create(
   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 
+  cancellations: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+  ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+  nisi ut aliquip ex ea commodo consequat.
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
   smtp_setting_attributes: {
     address: 'test', port: '25', domain_name: 'keppler.com',
     email: 'info@keppler.com', password: '12345678'
