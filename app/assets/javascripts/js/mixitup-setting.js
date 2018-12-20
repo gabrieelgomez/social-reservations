fe30346036625ae70cec097bfa3556313a91ad8f
LeonardJAvi <informatic.leonardavila@gmail.com>
2018-12-20 10:33:50 -0400

    fix cards filter

--- app/assets/javascripts/js/mixitup-setting.js
+++ app/assets/javascripts/js/mixitup-setting.js
@@ -1,21 +1,12 @@
-$(document).ready(function () {
-  $('#mix-wrapper').mixItUp({
-    load: {
-      sort: 'order:asc'
-    },
-    animation: {
-
-      duration: 700
-    },
-    selectors: {
-      target: '.mix-target',
-      filter: '.filter-btn',
-      sort: '.sort-btn'
-    },
-    callbacks: {
-      onMixEnd: function (state) {
-        console.log(state)
-      }
-    }
-  });
+mixitup('#mix-wrapper', {
+  load: {
+    sort: 'order:asc'
+  },
+  animation: {
+    effects: 'fade rotateZ(-180deg)',
+    duration: 700
+  },
+  selectors: {
+    target: '.mix-target'
+  }
 });
