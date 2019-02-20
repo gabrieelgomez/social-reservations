b07a35733476bfa222aa4ab710f3aee4f833f815
LeonardJAvi <informatic.leonardavila@gmail.com>
2019-02-20 16:04:32 -0400

    frontend fixes

--- app/assets/javascripts/js/mixitup-setting.js
+++ app/assets/javascripts/js/mixitup-setting.js
@@ -1,12 +1,17 @@
-mixitup('#mix-wrapper', {
-  load: {
-    sort: 'order:asc'
+var container = document.querySelector("#mix-wrapper");
+var config = {
+  controls: {
+    toggleDefault: "none"
   },
   animation: {
-    effects: 'fade rotateZ(-180deg)',
-    duration: 700
+    duration: 250,
+    nudge: true,
+    reverseOut: false,
+    effects: "fade translateZ(-100px)"
   },
   selectors: {
-    target: '.mix-target'
+    target: ".mix-target",
+    control: "[data-mixitup-control]"
   }
-});
+};
+var mixer = mixitup(container, config);
