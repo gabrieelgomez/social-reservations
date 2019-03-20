bdaa8bbc67a2d24326ee0477eaf0fe85ad31e8f7
gabrielgomez <gagg1707@gmail.com>
2019-03-20 13:10:33 -0400

    fix sintax user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def rol
-    roles.&first.&name
+    roles.first.&name
   end
 
   def permissions?
