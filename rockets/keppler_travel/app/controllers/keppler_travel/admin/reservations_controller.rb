7f9ef798f856b19ab2c3b403a8e3ab9e070f2eb3
gabrielgomez <gagg1707@gmail.com>
2019-03-24 14:41:01 -0400

    reject invoice nil x2

--- rockets/keppler_travel/app/controllers/keppler_travel/admin/reservations_controller.rb
+++ rockets/keppler_travel/app/controllers/keppler_travel/admin/reservations_controller.rb
@@ -32,11 +32,11 @@ module KepplerTravel
       # GET /reservations
       def index
         if @type_search == 'agency'
-          ids    = Order.where(details: 'agency').reject{|order| order.reservation.nil?}.collect{|order| order.reservation.id}
+          ids    = Order.where(details: 'agency').reject{|order| order.reservation.nil?}.reject{|order| order.reservation.invoice.nil?}.collect{|order| order.reservation.id}
         else
-          ids    = Order.where.not(details: 'agency').reject{|order| order.reservation.nil?}.collect{|order| order.reservation.id}
+          ids    = Order.where.not(details: 'agency').reject{|order| order.reservation.nil?}.reject{|order| order.reservation.invoice.nil?}.collect{|order| order.reservation.id}
         end
-        @types = Reservation.where(id: ids).where(reservationable_type: "KepplerTravel::#{@model}").reject{|reservation| reservation.invoice.nil?}
+        @types = Reservation.where(id: ids).where(reservationable_type: "KepplerTravel::#{@model}")
         @q = @types.ransack(params[:q])
         reservations = @q.result(distinct: true)
         @objects = reservations.page(@current_page).order(id: :desc)
