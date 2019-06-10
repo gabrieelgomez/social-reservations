88032d66e372f13e0500e6975c20046e5078f504
gabrielgomez <gagg1707@gmail.com>
2019-06-09 20:43:41 -0400

    remove create devise token auth

--- rockets/keppler_travel/app/decorators/controllers/devise_token_auth/sessions_controller_decorator.rb
+++ rockets/keppler_travel/app/decorators/controllers/devise_token_auth/sessions_controller_decorator.rb
@@ -4,28 +4,6 @@
 DeviseTokenAuth::SessionsController.class_eval do
 
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-
-
-    if current_user.has_role?(:agent) || current_user.has_role?(:agency)
-      agency = (current_user&.agency || current_user&.agent&.agency)
-      unless agency.unique_code.eql?(params[:agency_code])
-        sign_out
-        redirect_to new_user_session_path, flash: {error: "Código de agencia incorrecto"}
-      else
-        respond_with resource, location: after_sign_in_path_for(resource)
-      end
-    else
-      respond_with resource, location: after_sign_in_path_for(resource)
-    end
-
-
-  end
-
-  def create
     # Check
     field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
 
